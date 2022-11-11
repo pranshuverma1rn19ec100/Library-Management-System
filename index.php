f79ec6515613e6b48d56b390141c7aadec45c258
Md Akram Ali <1rn19ec100.pranshuverma@gmail.com>
2022-11-11 23:01:34 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/m1.css">
+    <title>Library Management Page</title>
+</head>
+<style>
+</style>
+<body>
+<div class="container">
+      <div class="glow">Lbrary Management System</div>
+    </div>
+    <button class="button-85" role="button">Click To See Our Collection</button>
+    <footer class="glow1">This is a Demo Project Made By Pranshu Verma</footer>
+ 
+</body>
+</html>
